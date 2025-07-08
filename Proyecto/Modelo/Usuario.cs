4ea5dff4ce81656e6dc70189d11e268d2cc2ab0f
MeliFJ <melifj2409@gmail.com>
2025-07-07 19:41:15 -0600

    nuevo formato

---
+++ Proyecto/Modelo/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto.Modelo
+{
+    public class Usuario
+    {
+        public string Identificacion { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        // Constructor
+        public Usuario(string identificacion, string password, string nombre, string apellido)
+        {
+            Identificacion = identificacion;
+            Password = password;
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+    }
+}
