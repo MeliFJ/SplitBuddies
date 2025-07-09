83eea40c480488498a73d065fe1fdf04ee6f9a05
yanicsacasco <ycasco@edu.upolitecnica.cr>
2025-07-09 17:12:04 -0600

    Se realizo cambios  y se completo correctamente el PBI1

--- Proyecto/Vista/RegistrarUsuario.cs
+++ Proyecto/Vista/RegistrarUsuario.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Projecto.Controlador;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Projecto.Modelo;
 
 namespace Projecto.Vista
 {
     public partial class RegistrarUsuario : Form
     {
+        private void Limpiar()
+        {
+            txtIdentificacion.Clear();
+            txtNombre.Clear();
+            TxtApellido.Clear();
+            txtContrasena.Clear();
+        }
         public RegistrarUsuario()
         {
             InitializeComponent();
@@ -36,8 +45,33 @@ namespace Projecto.Vista
         {
 
         }
-
+        
         private void btnguardar_Click(object sender, EventArgs e)
+        {
+            string Identificacion = txtIdentificacion.Text;
+            string nombre = txtNombre.Text;
+            string apellido = TxtApellido.Text;
+            string password = txtContrasena.Text;
+            GestorDatos gestorDatos = new GestorDatos();
+            Usuario NuevoUsuario = new Usuario (Identificacion,password,nombre,apellido);
+ 
+            var usuario = gestorDatos.BuscarUsuario(Identificacion);
+            if (usuario is null)
+            {
+                gestorDatos.GuardarUsuario(NuevoUsuario);
+                MessageBox.Show ( "Registro éxitoso","Registro de Usuario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Limpiar();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("El Usuario ya éxiste", "Registro de Usuario", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                Limpiar();
+                this.Close();
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
