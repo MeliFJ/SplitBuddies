b118526179daf0225f1a19b8babe375d03d5eb48
MeliFJ <melifj2409@gmail.com>
2025-07-11 16:25:43 -0600

    Nuevo formato

--- Proyecto/src/Controlador/LoginControlador.cs
+++ Proyecto/src/Controlador/LoginControlador.cs
@@ -11,9 +11,7 @@ namespace Projecto.Controlador
 
         public static bool ValidarLogin(string identificacion, string password)
         {
-            //Usuario nuevoUsuario = new Usuario("116640546", "1234", "Melissa", "Fallas");
             GestorDatos servicio = new GestorDatos();
-            //servicio.GuardarUsuario(nuevoUsuario);
 
             // Se busca si el usuario existe con la identificacion
             Usuario usuario=servicio.BuscarUsuario(identificacion);
